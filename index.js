200d0176f56d4ef164f6ffb61312aece269bc5a9
cs1812157 <98776469+cs1812157@users.noreply.github.com>
2022-08-30 15:49:20 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect("mongodb://localhost/membership-app");
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
